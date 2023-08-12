c1321d1c8e9b13dc8e80d6ece795e7d2f39b0f02
Charles Athande <ayomide4city@gmail.com>
2023-08-12 21:49:01 +0100

    first commit

---
+++ users/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import UserSerializer, TokenObtainPairSerializer
+
+class RegisterView(APIView):
+    http_method_names = ['post']
+    
+    def post(self, *args, **kwargs):
+        serializer = UserSerializer(data=self.request.data)
+        if serializer.is_valid():
+            get_user_model().objects.create_user(**serializer.validated_data)
+            return Response({'message':'User Created Successfully'},status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={'errors':serializer.errors})
+    
+
+class EmailTokenObtainPairView(TokenObtainPairView):
+    serializer_class = TokenObtainPairSerializer
